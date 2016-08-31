65aa53f94c60fb3980360807d5f6dbb17c3b6cd0
hugo.barrena <hugo.barrena@cespi.unlp.edu.ar>
2016-08-31 20:05:06 -0300

    Se agrego la libreria ja_serializers.

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,12 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+# Configures mime types
+config :phoenix, :format_encoders,
+  "json-api": Poison
+
+  config :mime, :types, %{
+  "application/vnd.api+json" => ["json-api"]
+
+  }
--- mix.exs
+++ mix.exs
@@ -37,7 +37,9 @@ defmodule TrackingServiceApi.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:ja_serializer, "~> 0.11.0"}
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
